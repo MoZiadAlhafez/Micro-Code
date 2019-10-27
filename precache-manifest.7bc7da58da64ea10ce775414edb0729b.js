c2eb2342f525490bd180eea1ed85ebaba7cd0c7c
Mo Ziad Alhafez <moziadalhafez@gmail.com>
2019-10-27 22:59:18 +0200

    Updates

---
+++ precache-manifest.7bc7da58da64ea10ce775414edb0729b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cde1060d3863928364f8b34a63bdcc0",
+    "url": "/Micro-Code/index.html"
+  },
+  {
+    "revision": "3dfb39991dde0706f8f6",
+    "url": "/Micro-Code/static/css/main.90424c52.chunk.css"
+  },
+  {
+    "revision": "4380f60571737ffcd329",
+    "url": "/Micro-Code/static/js/2.d49bc7aa.chunk.js"
+  },
+  {
+    "revision": "3dfb39991dde0706f8f6",
+    "url": "/Micro-Code/static/js/main.e4e95333.chunk.js"
+  },
+  {
+    "revision": "e12cd6378e7143c709cb",
+    "url": "/Micro-Code/static/js/runtime-main.81122787.js"
+  },
+  {
+    "revision": "3c860cfd34f20ffac1462966f1bd9f0b",
+    "url": "/Micro-Code/static/media/404.3c860cfd.svg"
+  },
+  {
+    "revision": "53203d09e7b7998338c5d7fca7f53a72",
+    "url": "/Micro-Code/static/media/Mobile application.53203d09.svg"
+  },
+  {
+    "revision": "84cbab4af458e875609e16cac72518f4",
+    "url": "/Micro-Code/static/media/analysis.84cbab4a.svg"
+  },
+  {
+    "revision": "21b338b24d4504490d8922e82bc7b753",
+    "url": "/Micro-Code/static/media/bug_fixing.21b338b2.svg"
+  },
+  {
+    "revision": "5d88a4acfea8aa17268f8bbe64d17011",
+    "url": "/Micro-Code/static/media/clean code.5d88a4ac.svg"
+  },
+  {
+    "revision": "a9cea6297990beccfb139307643812ea",
+    "url": "/Micro-Code/static/media/domain.a9cea629.svg"
+  },
+  {
+    "revision": "dfb5988f1b2fad5c1e6ee7882c977f25",
+    "url": "/Micro-Code/static/media/goals.dfb5988f.svg"
+  },
+  {
+    "revision": "5f2e49a4ba6e8ef5fb1533479908226e",
+    "url": "/Micro-Code/static/media/html css js.5f2e49a4.svg"
+  },
+  {
+    "revision": "f3cc1d4c5a57cb95c61374fe2e655168",
+    "url": "/Micro-Code/static/media/info.f3cc1d4c.svg"
+  },
+  {
+    "revision": "ef308b8d9a5b23e9afc25fb6c63129e7",
+    "url": "/Micro-Code/static/media/inspiration.ef308b8d.svg"
+  },
+  {
+    "revision": "3c44e615b579efb433f9555f1e9693f2",
+    "url": "/Micro-Code/static/media/laravel and vue.3c44e615.svg"
+  },
+  {
+    "revision": "6ddcfed11afaf89c03dd93d5543e3a41",
+    "url": "/Micro-Code/static/media/life.6ddcfed1.svg"
+  },
+  {
+    "revision": "e674f08278b736d3115350def530e950",
+    "url": "/Micro-Code/static/media/logo.e674f082.svg"
+  },
+  {
+    "revision": "1cbab5e226170f3a857d9376ac9ddfa2",
+    "url": "/Micro-Code/static/media/man1.1cbab5e2.jpg"
+  },
+  {
+    "revision": "ccd3e4df2278348049c57c277265acc4",
+    "url": "/Micro-Code/static/media/marketing.ccd3e4df.svg"
+  },
+  {
+    "revision": "4e85951fb9e94e713480390117546621",
+    "url": "/Micro-Code/static/media/responsive.4e85951f.svg"
+  },
+  {
+    "revision": "5866c947a09a836d5926e2b06602b28a",
+    "url": "/Micro-Code/static/media/social media.5866c947.svg"
+  },
+  {
+    "revision": "c9a3a05f35f9b88eac3f60942ecd0457",
+    "url": "/Micro-Code/static/media/under construction.c9a3a05f.svg"
+  }
+]);
